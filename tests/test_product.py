19145caaa59598f6b61575d3237098a12da66213
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-04-27 13:26:47 -0400

    feat: Agregar pruebas para la clase Product

---
+++ tests/test_product.py
@@ -0,0 +1,14 @@
+from oop_python_fundamentals.dunder_methods.product import Product
+
+
+def test_product_equality():
+    product_1 = Product("Curso de Python", 25000)
+    product_2 = Product("Curso de Python", 25000)
+
+    assert product_1 == product_2
+
+
+def test_product_str():
+    product = Product("Curso de Python", 25000)
+
+    assert str(product) == "Producto: Curso de Python - Precio: $25000"
