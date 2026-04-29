bb09fa80db7dbfd052efd772ea821ac81967439e
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-04-29 13:42:33 -0400

    feat: agregar pruebas unitarias para la clase Computer

---
+++ tests/test_computer.py
@@ -0,0 +1,24 @@
+from oop_python_fundamentals.composition.computer import Computer, Processor, Memory
+
+
+def test_computer_builds_composed_objects():
+    computer = Computer("Intel i7", "16 GB")
+
+    assert isinstance(computer.processor, Processor)
+    assert isinstance(computer.memory, Memory)
+    assert computer.processor.model == "Intel i7"
+    assert computer.memory.size == "16 GB"
+
+
+def test_computer_show_specs_prints_expected_output(capsys):
+    computer = Computer("Intel i7", "16 GB")
+
+    computer.show_specs()
+
+    captured = capsys.readouterr()
+    output_lines = captured.out.strip().splitlines()
+
+    assert output_lines == [
+        "Procesador: Intel i7",
+        "Memoria RAM: 16 GB",
+    ]
