a9d132d0bd715a4a388a925acf616a27b24b85ed
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-05 14:24:37 -0400

    feat: agregar clase ProductWithProperty para manejar precios con @property y setters

---
+++ src/oop_python_fundamentos/encapsulamiento/product_with_property.py
@@ -0,0 +1,25 @@
+class ProductWithProperty:
+    """Clase para explicar el uso de @property y setters."""
+
+    def __init__(self, name: str, price: float):
+        self.name = name
+        self.__price = 0
+        self.price = price
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, value: float) -> None:
+        if value <= 0:
+            raise ValueError("El precio debe ser mayor a cero")
+
+        self.__price = value
+
+
+if __name__ == "__main__":
+    product = ProductWithProperty("Curso de Python", 25000)
+    print(product.price)
+    product.price = 30000
+    print(product.price)
