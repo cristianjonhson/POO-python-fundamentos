c7c109f874edfd89d8a8f33edfa8491c81f69b8a
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-06 14:27:39 -0400

    fix: corregir la importación del módulo de bank_account en los tests

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -1,6 +1,6 @@
 import pytest
 
-from oop_python_fundamentos.encapsulation.bank_account import BankAccount
+from oop_python_fundamentos.encapsulamiento.bank_account import BankAccount
 
 
 def test_deposit_increases_balance():
