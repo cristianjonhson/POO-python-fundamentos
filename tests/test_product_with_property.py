71713bd5c6da8ebf667b300e5c543b79df1aac90
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-06 14:28:36 -0400

    fix: corregir la importación del módulo de product_with_property en los tests

--- tests/test_product_with_property.py
+++ tests/test_product_with_property.py
@@ -1,6 +1,6 @@
 import pytest
 
-from oop_python_fundamentos.encapsulation.product_with_property import ProductWithProperty
+from oop_python_fundamentos.encapsulamiento.product_with_property import ProductWithProperty
 
 
 def test_product_with_property_initializes_price():
